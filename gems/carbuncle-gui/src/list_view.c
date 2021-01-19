e8191cfb39ea2eb6e005c838b3ed2d4dd9cb4104
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2021-01-19 13:37:45 -0300

    Add list view component

--- gems/carbuncle-gui/src/list_view.c
+++ gems/carbuncle-gui/src/list_view.c
@@ -16,7 +16,49 @@
 
 #include <raylib.h>
 
+#define LIST_VIEW_KEYS 13
+
+static const char *LIST_VIEW_KEYWORDS[LIST_VIEW_KEYS] = {
+  "border", "movable", "scalable", "closable", "minimizable",
+  "disable_scrollbar", "title", "auto_scrollbar", "background", "scale_left", "disable",
+  "minimized", "maximized"  
+};
+
+static mrb_value
+gui_list_view(mrb_state *mrb, mrb_value self)
+{
+  mrb_value kw_values[LIST_VIEW_KEYS];
+  mrb_value block;
+  const char *id;
+  mrb_int height, rows;
+  struct nk_list_view view;
+  nk_flags flags = 0;
+  const mrb_kwargs kwargs = { LIST_VIEW_KEYS, kw_values, LIST_VIEW_KEYWORDS, 0, NULL };  
+  mrb_get_args(mrb, "zii:&", &id, &height, &rows, &kwargs, &block);
+  if (mrb_carbuncle_test(kw_values[0]))  { flags |= NK_WINDOW_BORDER; }
+  if (mrb_carbuncle_test(kw_values[1]))  { flags |= NK_WINDOW_MOVABLE; }
+  if (mrb_carbuncle_test(kw_values[2]))  { flags |= NK_WINDOW_SCALABLE; }
+  if (mrb_carbuncle_test(kw_values[3]))  { flags |= NK_WINDOW_CLOSABLE; }
+  if (mrb_carbuncle_test(kw_values[4]))  { flags |= NK_WINDOW_MINIMIZABLE; }
+  if (mrb_carbuncle_test(kw_values[5]))  { flags |= NK_WINDOW_NO_SCROLLBAR; }
+  if (mrb_carbuncle_test(kw_values[6]))  { flags |= NK_WINDOW_TITLE; }
+  if (mrb_carbuncle_test(kw_values[7]))  { flags |= NK_WINDOW_SCROLL_AUTO_HIDE; }
+  if (mrb_carbuncle_test(kw_values[8]))  { flags |= NK_WINDOW_BACKGROUND; }
+  if (mrb_carbuncle_test(kw_values[9])) { flags |= NK_WINDOW_SCALE_LEFT; }
+  if (mrb_carbuncle_test(kw_values[10])) { flags |= NK_WINDOW_NO_INPUT; }
+  if (mrb_carbuncle_test(kw_values[11])) { flags |= NK_MINIMIZED; }
+  if (mrb_carbuncle_test(kw_values[12])) { flags |= NK_MAXIMIZED; }  
+  struct mrb_GuiContext *ctx = mrb_carbuncle_gui_get_context(mrb, self); 
+  if (nk_list_view_begin(&(ctx->nk), &view, id, flags, height, rows))
+  {
+    mrb_yield_argv(mrb, block, 0, NULL);
+    nk_list_view_end(&(ctx->nk));
+  }
+  return self;
+}
+
 void
 mrb_init_carbuncle_gui_list_view(mrb_state *mrb, struct RClass *gui)
 {
+  mrb_define_method(mrb, gui, "list_view", gui_list_view, MRB_ARGS_BLOCK());
 }
