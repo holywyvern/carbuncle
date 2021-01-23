45cab7c7ca83237293d184c258b7486ef9d6f22f
holywyvern <ramiro.rojo.cretta@gmail.com>
2021-01-23 19:32:15 +0000

    Deploying to gh-pages from  @ 4f2e7f2fbd03d0d053dfeb5caea2304350e48070 🚀

--- _next/static/JfMGIjAamH-x5qFUHyyEQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-5d04743a3e8322a97127.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e2ffa4674eea62b50f3f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
