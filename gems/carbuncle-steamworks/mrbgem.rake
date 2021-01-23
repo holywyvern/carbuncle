76394ec66f9f73f1c7abc87f49fc22d1550f5b3b
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2021-01-22 23:35:34 -0300

    Add steam api bases

---
+++ gems/carbuncle-steamworks/mrbgem.rake
@@ -0,0 +1,5 @@
+MRuby::Gem::Specification.new('carbuncle-steamworks') do |spec|
+  spec.license = 'MIT'
+  spec.author  = 'Ramiro Rojo'
+  spec.summary = 'Steamworks SDK integration'
+end
---
+++ gems/carbuncle-steamworks/src/main.c
@@ -0,0 +1,121 @@
+#include <mruby.h>
+#include <mruby/error.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include <carbuncle/core.h>
+
+#ifdef _WIN32
+# include <windows.h>
+# define LIBRARY_NAME "steam_api.dll"
+typedef HMODULE LibraryHandle;
+typedef FARPROC LibraryFunction;
+#else
+# include <dlfcn.h>
+typedef void* LibraryHandle;
+typedef void* LibraryFunction;
+
+# ifdef __unix__
+#   define LIBRARY_NAME "libsteam_api.so"
+# elif __APPLE__
+#   define LIBRARY_NAME "libsteam_api.dylib"
+# endif
+#endif
+
+static LibraryHandle mrb_carbuncle_steamworks_handle;
+
+static bool (*steamworks_Init)(void);
+static bool (*steamworks_RestartAppIfNecessary)(uint32_t);
+static void (*steamworks_Shutdown)(void);
+
+static void
+open_library(mrb_state *mrb)
+{
+#ifdef _WIN32
+  mrb_carbuncle_steamworks_handle = LoadLibraryA(LIBRARY_NAME);
+#else
+  mrb_carbuncle_steamworks_handle = dlopen(LIBRARY_NAME, RTLD_LAZY);
+#endif
+ if (!mrb_carbuncle_steamworks_handle)
+ {
+   mrb_raise(mrb, E_RUNTIME_ERROR, "Failed to load steamworks API");
+ }
+}
+
+static LibraryFunction
+load_function(mrb_state *mrb, const char *name)
+{
+  LibraryFunction result;
+#ifdef _WIN32
+  result = GetProcAddress(mrb_carbuncle_steamworks_handle, name);
+#elif __unix__
+  result = dlsym(mrb_carbuncle_steamworks_handle, name);
+#endif
+  if (!result)
+  {
+    mrb_raisef(mrb, E_RUNTIME_ERROR, "Failed to load steamworks API function %s", name);
+  }
+}
+
+static void
+open_library(mrb_state *mrb)
+{
+  steamworks_RestartAppIfNecessary = load_function(mrb, "SteamAPI_RestartAppIfNecessary");
+  steamworks_Init = load_function(mrb, "SteamAPI_Init");
+  steamworks_Shutdown = load_function(mrb, "SteamAPI_Shutdown");
+}
+
+static mrb_value
+steam_load(mrb_state *mrb, mrb_value self)
+{
+  mrb_int app_id;
+  mrb_get_args(mrb, "i", &app_id);
+  /* Already loaded, skip */
+  if (mrb_carbuncle_steamworks_handle)
+  {
+    return self;
+  }
+  open_library(mrb);
+  load_functions(mrb);
+  if (steamworks_RestartAppIfNecessary(app_id))
+  {
+    exit(EXIT_FAILURE);
+  }
+  if (!steamworks_Init())
+  {
+    mrb_raise(mrb, E_RUNTIME_ERROR, "Failed to initialize steamworks API");
+  }
+  return self;
+}
+
+static mrb_value
+steam_unload(mrb_state *mrb, mrb_value self)
+{
+  if (mrb_carbuncle_steamworks_handle)
+  {
+#ifdef _WIN32
+    FreeLibrary(mrb_carbuncle_steamworks_handle);
+#else
+    dlclose(mrb_carbuncle_steamworks_handle);
+    mrb_carbuncle_steamworks_handle = NULL;
+#endif
+    mrb_carbuncle_steamworks_handle = NULL;
+  }
+  return self;
+}
+
+void
+mrb_carbuncle_steamworks_gem_init(mrb_state *mrb)
+{
+  struct RClass *steam = mrb_define_module(mrb, "Steamworks");
+
+  mrb_define_module_function(mrb, steam, "load", steam_load, MRB_ARGS_REQ(1));
+  mrb_define_module_function(mrb, steam, "unload", steam_unload, MRB_ARGS_NONE());
+}
+
+void
+mrb_carbuncle_steamworks_gem_final(mrb_state *mrb)
+{
+}
