148f680b51d92d96155117027860a247860f7780
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2021-01-24 22:41:36 -0300

    add base path to load path

--- website/i18n.js
+++ website/i18n.js
@@ -2,6 +2,8 @@ const NextI18Next = require('next-i18next').default;
 const { localeSubpaths } = require('next/config').default().publicRuntimeConfig;
 const path = require('path');
 
+const basePath = process.env.NODE_ENV === 'production' ? '/carbuncle' : '';
+
 module.exports = new NextI18Next({
   whitelist: ['en', 'es'],
   otherLanguages: ['es'],
@@ -9,5 +11,11 @@ module.exports = new NextI18Next({
   localePath: path.resolve('./public/static/locales'),
   saveMissingTo: "current",
   fallbackLng: process.NODE_ENV !== 'production' ? false : 'en',
-  saveMissing: process.NODE_ENV !== 'production'
+  saveMissing: process.NODE_ENV !== 'production',
+  detection: {
+    order: ['querystring', 'localStorage', 'cookie', 'navigator', 'htmlTag'],
+  },
+  backend: {
+    loadPath: `${basePath}/static/locales/{{lng}}/{{ns}}.json`,
+  },
 })
